fa2ba96cd28e74345e5b483b3b9367214a4f7419
Vishal Chudashama <vishal@arkinfosoft.com>
2020-09-07 22:30:36 +0530

    Changes in MailSend API

--- DoctorDiaryAPI/csfiles/Email.cs
+++ DoctorDiaryAPI/csfiles/Email.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Net;
+using System.Net.Configuration;
 using System.Net.Mail;
 using System.Text;
 
@@ -13,50 +15,73 @@ namespace DoctorDiaryAPI
         {
             try
             {
-                //var fromAddress = "ajaysinh.mail@gmail.com";  
-                //var fromAddress = "harshal@arkinfosoft.com";
-                var fromAddress = "doctordairy@arkinfosoft.com";
-                var toAddress = ToAdd;
-                //const string fromPassword = "Ajay@123456";  
-                const string fromPassword = "Harshal@123";
-                string subject = emailSubject;
-                string body = emailBody;
-                System.Net.Mail.MailMessage email = new System.Net.Mail.MailMessage();
-                if (filename != "")
-                {
-                    System.Net.Mail.Attachment attachment;
-                    attachment = new System.Net.Mail.Attachment(filename);
-                    email.Attachments.Add(attachment);
-                }
-               
-                email.To.Add(toAddress);
-                email.From = new MailAddress(fromAddress);
-                email.Subject = emailSubject;
-                email.Body = emailBody;
-                email.IsBodyHtml = true;
-                //if (cc != "" && cc!= "noreply.allowme@gmail.com" && cc!= "noreply@allowme-admin.com")
-                //{
-                //    email.CC.Add("matthew@allow-me.com.au");
-                //    email.CC.Add(cc);
-                //}               
+                SmtpSection smtpSettings = (SmtpSection)ConfigurationManager.GetSection("system.net/mailSettings/smtp");
 
-                var smtp = new System.Net.Mail.SmtpClient();
+                var fromAddress = smtpSettings.From;
+
+                //created object of SmtpClient details and provides server details
+                using (SmtpClient smtp = new SmtpClient())
                 {
-                    smtp.Host = "smtp.gmail.com";
                     //smtp.Host = "mail.arkinfosoft.com";
-                    smtp.Port = 587;// 25;
-                    smtp.EnableSsl = true;
-                    smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-                    smtp.Credentials = new NetworkCredential(fromAddress, fromPassword);
-                    smtp.UseDefaultCredentials = true;
-                    //smtp.Timeout = 20000;
+                    smtp.Host = smtpSettings.Network.Host;
+                    smtp.Port = smtpSettings.Network.Port;
+                    smtp.EnableSsl = smtpSettings.Network.EnableSsl;
+                    smtp.DeliveryMethod = smtpSettings.DeliveryMethod;
+                    smtp.UseDefaultCredentials = smtpSettings.Network.DefaultCredentials;
 
+                    //Server Credentials
+                    NetworkCredential NC = new NetworkCredential();
+                    NC.UserName = smtpSettings.Network.UserName;
+                    NC.Password = smtpSettings.Network.Password;
+
+                    //assigned credetial details to server
+                    smtp.Credentials = NC;
+
+                    //create sender address
+                    MailAddress from = new MailAddress(fromAddress, "Doctor Diary");
+
+                    //if (fromEmail != "")
+                    //{
+                    //    from = new MailAddress(fromAddress, "App User");
+                    //}
+
+                    //create receiver address
+                    MailAddress receiver = new MailAddress(ToAdd, "Testing");
+
+                    MailMessage message = new MailMessage(from, receiver);
+                    message.Subject = emailSubject.Trim();
+                    message.Body = emailBody.Trim();
+                    message.IsBodyHtml = true;
+
+                    if (filename != "")
+                    {
+                        System.Net.Mail.Attachment attachment;
+                        attachment = new System.Net.Mail.Attachment(filename);
+                        message.Attachments.Add(attachment);
+                    }
+
+                    if (cc != "")
+                    {
+                        if (cc.Contains('|'))
+                        {
+                            var emails = cc.Split('|');
+
+                            foreach (string emailAddress in emails)
+                            {
+                                message.CC.Add(emailAddress.Trim());
+                            }
+                        }
+                        else if (cc.Contains('@'))
+                        {
+                            message.CC.Add(cc.Trim());
+                        }
+                    }
+
+                    //sends the email
+                    smtp.Send(message);
+
+                    return true;
                 }
-                smtp.Send(email);
-
-            
-                return true;
-
             }
             catch (Exception ex)
             {
@@ -64,6 +89,60 @@ namespace DoctorDiaryAPI
                 ErrHandler.WriteError(ex.Message, ex);
                 return false;
             }
+
+            //try
+            //{
+            //    //var fromAddress = "ajaysinh.mail@gmail.com";  
+            //    //var fromAddress = "harshal@arkinfosoft.com";
+            //    var fromAddress = "doctordairy@arkinfosoft.com";
+            //    var toAddress = ToAdd;
+            //    //const string fromPassword = "Ajay@123456";  
+            //    const string fromPassword = "Harshal@123";
+            //    string subject = emailSubject;
+            //    string body = emailBody;
+            //    System.Net.Mail.MailMessage email = new System.Net.Mail.MailMessage();
+            //    if (filename != "")
+            //    {
+            //        System.Net.Mail.Attachment attachment;
+            //        attachment = new System.Net.Mail.Attachment(filename);
+            //        email.Attachments.Add(attachment);
+            //    }
+
+            //    email.To.Add(ToAdd);
+            //    email.From = new MailAddress(fromAddress);
+            //    email.Subject = emailSubject;
+            //    email.Body = emailBody;
+            //    email.IsBodyHtml = true;
+            //    //if (cc != "" && cc!= "noreply.allowme@gmail.com" && cc!= "noreply@allowme-admin.com")
+            //    //{
+            //    //    email.CC.Add("matthew@allow-me.com.au");
+            //    //    email.CC.Add(cc);
+            //    //}               
+
+            //    var smtp = new System.Net.Mail.SmtpClient();
+            //    {
+            //        smtp.Host = "smtp.gmail.com";
+            //        //smtp.Host = "mail.arkinfosoft.com";
+            //        smtp.Port = 587;// 25;
+            //        smtp.EnableSsl = true;
+            //        smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+            //        smtp.Credentials = new NetworkCredential(fromAddress, fromPassword);
+            //        smtp.UseDefaultCredentials = true;
+            //        //smtp.Timeout = 20000;
+
+            //    }
+            //    smtp.Send(email);
+
+
+            //    return true;
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    //Console.WriteLine(ex.ToString());
+            //    ErrHandler.WriteError(ex.Message, ex);
+            //    return false;
+            //}
         }
     }
 }
--- DoctorDiaryAPI/csfiles/SmsSend.cs
+++ DoctorDiaryAPI/csfiles/SmsSend.cs
@@ -10,29 +10,39 @@ namespace DoctorDiaryAPI
 {
     public class SmsSend
     {
-         public static bool Send(string MobileNo, string Message)
+        public static bool Send(string MobileNo, string Message)
         {
-            string smsstr = "http://msg.msgclub.net/rest/services/sendSMS/sendGroupSms?AUTH_KEY=bf61de676eda2e011125d528133625f&message=" + Message + "&senderId=DOCDIR&routeId=1&mobileNos="+ MobileNo +"&smsContentType=english";
-            //string smsstr = "http://sms.myepicsoft.com/rest/services/sendSMS/sendGroupSms?AUTH_KEY=d2ba29c1a3d8e7ca4e4dfffd5e24070&message=" + Message + "&senderId=ECOUNT&routeId=1&mobileNos=" + MobileNo + "&smsContentType=english";
-            HttpWebRequest _createRequest = (HttpWebRequest)WebRequest.Create(smsstr);
-            //getting response of sms
-            HttpWebResponse myResp = (HttpWebResponse)_createRequest.GetResponse();
-            System.IO.StreamReader _responseStreamReader = new System.IO.StreamReader(myResp.GetResponseStream());
-
-            string responseString = _responseStreamReader.ReadToEnd();
-            System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
-
-            System.Web.Script.Serialization.JavaScriptSerializer js = new JavaScriptSerializer();
-            SMSResponse response = (SMSResponse)js.Deserialize(responseString, typeof(SMSResponse));            
-            
-            _responseStreamReader.Close();
-            myResp.Close();
-            if (response.responseCode == "3001")
+            try
             {
-                return true;
+
+                string smsstr = "http://msg.msgclub.net/rest/services/sendSMS/sendGroupSms?AUTH_KEY=bf61de676eda2e011125d528133625f&message=" + Message + "&senderId=DOCDIR&routeId=1&mobileNos=" + MobileNo + "&smsContentType=english";
+                //string smsstr = "http://sms.myepicsoft.com/rest/services/sendSMS/sendGroupSms?AUTH_KEY=d2ba29c1a3d8e7ca4e4dfffd5e24070&message=" + Message + "&senderId=ECOUNT&routeId=1&mobileNos=" + MobileNo + "&smsContentType=english";
+                HttpWebRequest _createRequest = (HttpWebRequest)WebRequest.Create(smsstr);
+                //getting response of sms
+                HttpWebResponse myResp = (HttpWebResponse)_createRequest.GetResponse();
+                System.IO.StreamReader _responseStreamReader = new System.IO.StreamReader(myResp.GetResponseStream());
+
+                string responseString = _responseStreamReader.ReadToEnd();
+                System.Web.Script.Serialization.JavaScriptSerializer serializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+
+                System.Web.Script.Serialization.JavaScriptSerializer js = new JavaScriptSerializer();
+                SMSResponse response = (SMSResponse)js.Deserialize(responseString, typeof(SMSResponse));
+
+                _responseStreamReader.Close();
+                myResp.Close();
+                if (response.responseCode == "3001")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
             }
-            else
+            catch (Exception ex)
             {
+                ErrHandler.WriteError(ex.Message, ex);
                 return false;
             }
         }
