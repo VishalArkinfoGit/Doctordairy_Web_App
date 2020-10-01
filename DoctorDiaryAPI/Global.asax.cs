1175d31cac9fc739b87e4cd30b4bfe62b0483f8b
Vishal Chudashama <vishal@arkinfosoft.com>
2020-10-01 12:35:11 +0530

    Modify Unauthorized response message

--- DoctorDiaryAPI/Global.asax.cs
+++ DoctorDiaryAPI/Global.asax.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using DoctorDiaryAPI.Controllers;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Web;
 using System.Web.Http;
@@ -30,6 +34,30 @@ namespace DoctorDiaryAPI
             newCulture.DateTimeFormat.ShortDatePattern = "dd/MM/yyyy";
             newCulture.DateTimeFormat.DateSeparator = "-";
             Thread.CurrentThread.CurrentCulture = newCulture;
+        }
+
+        protected void Application_PostAuthorizeRequest()
+        {
+            System.Web.HttpContext.Current.SetSessionStateBehavior(System.Web.SessionState.SessionStateBehavior.Required);
+        }
+
+        protected void Application_EndRequest()
+        {
+            var context = new HttpContextWrapper(Context);
+
+            var content = new ReturnObject()
+            {
+                message = "You are not Authorized to perform this action!",
+                status_code = Convert.ToInt32(HttpStatusCode.Unauthorized)
+            };
+
+            if (context.Response.StatusCode == 401)
+            {
+                HttpContext.Current.Response.ClearContent();
+
+                context.Response.StatusCode = Convert.ToInt32(HttpStatusCode.Unauthorized);
+                context.Response.Write(JsonConvert.SerializeObject(content));
+            }
         }
     }
 }
